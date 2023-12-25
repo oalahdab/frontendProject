eff3bd47038099859300e96a90b6d2d8ecc8cd04
YourName <o.alahdab81@gmail.com>
2023-12-25 11:42:40 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Örnek bir JavaScript fonksiyonu
+function showMessage() {
+  alert("Butona tiklandi!");
+}
+
+// Örneğin, bir butona tiklandiğinda showMessage fonksiyonunu çağirma
+const btn = document.getElementById("a");
+btn.addEventListener("click", () => showMessage());
+
