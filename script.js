50f39ea7bb0fbd4c1093be36627801ce6c6fbe11
Obada-JL <leader.obada@gmail.com>
2023-12-26 18:43:45 +0300

    ggggg

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
-// script.js
+let slideIndex = 1;
+showSlides(slideIndex);
 
-// Örnek bir JavaScript fonksiyonu
-function showMessage() {
-  alert("Butona tiklandi!");
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
 }
 
-// Örneğin, bir butona tiklandiğinda showMessage fonksiyonunu çağirma
-const btn = document.getElementById("a");
-btn.addEventListener("click", () => showMessage());
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
 
-console.log("hello");
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
